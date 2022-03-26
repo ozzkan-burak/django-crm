229bddbfaf6c415485da2019833a3fc5c0319c61
Burak ÖZKAN <burak.ozkan@flo.com.tr>
2022-03-26 18:14:52 +0300

    episode2

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import AbstractUSer
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+
+
+class Lead(models.Model):
+  first_name = models.CharField(max_length=20)
+  last_name = models.CharField(max_length=20)
+  age = models.IntegerField(default=0)
+  agent = models.ForeignKey('Agent', on_delete=models.CASCADE, null=True, blank=True)
+
+
+class Agent(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  first_name = models.CharField(max_length=20)
+  last_name = models.CharField(max_length=20)
