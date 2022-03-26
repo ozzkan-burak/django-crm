2cb2f898a624300e386fd1a0ccf96d9a97a5daf2
Burak ÖZKAN <burak.ozkan@flo.com.tr>
2022-03-26 18:48:30 +0300

    episode3

--- leads/models.py
+++ leads/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth import AbstractUSer
+from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
@@ -13,6 +13,4 @@ class Lead(models.Model):
 
 
 class Agent(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  first_name = models.CharField(max_length=20)
-  last_name = models.CharField(max_length=20)
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
